9dc95aab3b32a546d44c9b0d8a56f7b97fa98741
g-clef <g-clef@users.noreply.github.com>
2020-03-11 13:41:18 -0400

    Update to hooks (#1)
    * Adding hook for mysql-relation-joined, instead of mysql.available

--- reactive/nextcloud.py
+++ reactive/nextcloud.py
@@ -1,44 +1,38 @@
 
-from charms.reactive import when, when_not, set_flag
+from charms.reactive import when, when_not, set_flag, hook
 
-from charmhelpers.core import unitdata
+# from charmhelpers.core import unitdata
 from charmhelpers.core.hookenv import open_port, status_set, config, unit_public_ip, log
-from charmhelpers.core.host import chdir, service_restart
-from charmhelpers.core.templating import render
+from charmhelpers.core.host import chdir
+# from charmhelpers.core.host import service_restart
+# from charmhelpers.core.templating import render
 
 from pathlib import Path
 import subprocess
 
 
-#@when('mysql.connected')
-#@when_not('nextcloud.dbrequested')
-#def request_nextcloud_database(mysql):
-#
-#    status_set('maintenance',
-#               'Requesting MySQL database for Nextcloud')
-#
-#    mysql.configure('nextcloud_db')
-#
-#    status_set('active', 'Nextcloud database requested')
-#    set_flag('nextcloud.dbrequested')
+@hook("mysql-relation-joined")
+def db_ready(_):
+    status_set("blocked", "Database joined but not configured")
+    set_flag("nextcloud.db_ready")
 
 
-@when('mysql.available')
+@when('nextcloud.db_ready', 'mysql.available')
 @when_not('nextcloud.initdone')
 def init_nextcloud(mysql):
 
     status_set('maintenance', "Initializing Nextcloud")
 
-    ctxt = { 'dbname': mysql.database(),
-             'dbuser': mysql.user(),
-             'dbpass': mysql.password(),
-             'dbhost': mysql.host(),
-             'dbport': mysql.port(),
-             'dbtype': 'mysql',
-             'admin_username': config().get('admin-username'),
-             'admin_password': config().get('admin-password'),
-             'data_dir': Path('/var/www/nextcloud/data'),
-             }
+    ctxt = {'dbname': mysql.database(),
+            'dbuser': mysql.user(),
+            'dbpass': mysql.password(),
+            'dbhost': mysql.host(),
+            'dbport': mysql.port(),
+            'dbtype': 'mysql',
+            'admin_username': config().get('admin-username'),
+            'admin_password': config().get('admin-password'),
+            'data_dir': Path('/var/www/nextcloud/data'),
+            }
 
     nextcloud_init = ("sudo -u www-data /usr/bin/php occ  maintenance:install "
                       "--database {dbtype} --database-name {dbname} "
@@ -50,7 +44,7 @@ def init_nextcloud(mysql):
     log(nextcloud_init)
 
     with chdir('/var/www/nextcloud'):
-        subprocess.call(("sudo chown -R www-data:www-data .").split())
+        subprocess.call("sudo chown -R www-data:www-data .".split())
         subprocess.call(nextcloud_init.split())
 
     Path('/var/www/nextcloud/config/config.php').write_text(
@@ -78,9 +72,8 @@ def server_config():
     status_set('active', "Ready")
 
 
-@when_not('mysql.available')
+@when_not('nextcloud.db_ready')
 def blocked_on_mysql():
 
     status_set('blocked', "Need connection to MySQL to continue")
     return
-
